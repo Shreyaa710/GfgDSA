9d8681ebe74b74c52e82872acecaad67bd12d251
Shreyaa710 <shreyashar80@gmail.com>
2023-08-01 21:50:28 +0530

    yo

---
+++ Strings/chk_string_rotation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isRotated(string &s1, string &s2)
+{
+    if(s1.length() != s2.length()) return false;
+
+    return ((s1+s2).find(s2) != string::npos);
+}
+
+int main()
+{
+    string s1="dabbas";
+    string s2="asda";
+    cout<<isRotated(s1,s2);
+}
